4fb48581b3fb832f26d489ca1e6f3a4c15c206e8
DenisBilokon <Superden994@gmail.com>
2024-03-19 20:11:23 +0200

    Добавьте файлы проекта.

---
+++ SecondLab/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class BankAccount
+{
+    private readonly string accountNumber;
+    private double balance;
+
+    public BankAccount(string accountNumber, double initialBalance)
+    {
+        this.accountNumber = accountNumber;
+        balance = initialBalance;
+    }
+
+    public void Deposit(double amount)
+    {
+        balance += amount;
+    }
+
+    public bool Withdraw(double amount)
+    {
+        if (amount <= balance)
+        {
+            balance -= amount;
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Insufficient funds.");
+            return false;
+        }
+    }
+
+    public void ShowAccountInfo()
+    {
+        Console.WriteLine($"Account Number: {accountNumber}");
+        Console.WriteLine($"Balance: {balance}");
+    }
+}
---
+++ SecondLab/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount account = new BankAccount("123456789", 1000.0);
+
+        account.ShowAccountInfo();
+        Console.Write("\n");
+
+        account.Deposit(500.0);
+        Console.Write("Deposited 500.0\n");
+
+        account.ShowAccountInfo();
+        Console.Write("\n");
+
+        account.Withdraw(200.0);
+        Console.Write("Withdrawn 200.0\n");
+
+        account.ShowAccountInfo();
+        Console.Write("\n");
+
+        account.Withdraw(2000.0);
+        Console.Write("Attempted to withdraw 2000.0\n");
+
+        account.ShowAccountInfo();
+        Console.ReadLine();
+    }
+}
